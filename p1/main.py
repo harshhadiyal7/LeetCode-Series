0c04055d8286116aa76de83df33d0bb3009e70e4
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-02-14 15:58:39 +0530

    Two sum

--- p1/main.py
+++ p1/main.py
@@ -1,5 +1,10 @@
 #Two sum
-
+'''
+concept : 
+nums = [5,9,1,2,4,15,6,3]
+return index of target sum 2 numbers.
+for ex: target=13  and 9 and 4 are numbers and we resturn index of 9 and 4. it means [1,4].
+'''
 class Solution:
     def twoSum(self, nums, target):
         seen = {}
