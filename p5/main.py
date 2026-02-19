f2240051244a5de8308c6036228fb108963a94a0
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-02-19 11:16:18 +0530

    2. Add Two Numbers

--- p5/main.py
+++ p5/main.py
@@ -22,6 +22,35 @@ Constraints:
 The number of nodes in each linked list is in the range [1, 100].
 0 <= Node.val <= 9
 It is guaranteed that the list represents a number that does not have leading zeros.
- 
-
 '''
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def addTwoNumbers(self, l1, l2):
+        """
+        :type l1: Optional[ListNode]
+        :type l2: Optional[ListNode]
+        :rtype: Optional[ListNode]
+        """
+        dummy = ListNode(0)
+        current = dummy
+        carry = 0
+        while l1 or l2 or carry:
+            v1 = l1.val if l1 else 0
+            v2 = l2.val if l2 else 0
+            
+            total = v1 + v2 + carry
+            carry = total // 10
+            val = total % 10
+            
+            current.next = ListNode(val)
+            current = current.next
+            
+            l1 = l1.next if l1 else None
+            l2 = l2.next if l2 else None
+            
+        return dummy.next
+        
