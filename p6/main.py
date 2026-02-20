59adf4d4bc2f27edf83d930af3debabcecda123f
Harsh Hadiyal <harshhadiyal7733@gmail.com>
2026-02-20 12:43:53 +0530

    189. Rotate Array(Medium)

--- p6/main.py
+++ p6/main.py
@@ -26,3 +26,28 @@ Constraints:
 0 <= k <= 105
  
 '''
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k %= n  # Handle cases where k > array length
+        
+        # Helper function to reverse a portion of the array
+        def reverse(start, end):
+            while start < end:
+                nums[start], nums[end] = nums[end], nums[start]
+                start += 1
+                end -= 1
+        
+        # 1. Reverse the whole thing: [1,2,3,4,5,6,7] -> [7,6,5,4,3,2,1]
+        reverse(0, n - 1)
+        
+        # 2. Reverse the first k elements: [7,6,5, 4,3,2,1] -> [5,6,7, 4,3,2,1]
+        reverse(0, k - 1)
+        
+        # 3. Reverse the rest: [5,6,7, 4,3,2,1] -> [5,6,7, 1,2,3,4]
+        reverse(k, n - 1)
